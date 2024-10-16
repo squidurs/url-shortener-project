cd21b9f71c712d497136d0d977696ced105cd1a2
Sydney McClements <smccl96@gmail.com>
2024-10-15 17:20:08 -0700

    Add negative test cases, enhance URL validation, enforce custom URL length, improve UUID usage, and add docstrings for API documentation.

--- models/url_pydantic_models.py
+++ models/url_pydantic_models.py
@@ -1,13 +1,13 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, HttpUrl, Field
 from datetime import datetime
 from typing import Optional
 # Define your models here
 
 
 class URLRequest(BaseModel):
-    url: str
-    custom_url: Optional[str] = None
-    length: int = 6 #do i need to enforce a minimum? max?
+    url: HttpUrl
+    custom_url: Optional[str] = Field(default=None, min_length=10, max_length=15, description="Length must be between 10 and 15")
+    length: int = Field(default=10, ge=10, le=15, description="Length must be between 10 and 15")
 
 
 class URLResponse(BaseModel):
--- tests/test_url_shortener.py
+++ tests/test_url_shortener.py
@@ -1,8 +1,10 @@
 import unittest
-from unittest.mock import patch, MagicMock
+from unittest.mock import patch
 from models.pynamodb_model import UrlEntry
+from models.url_pydantic_models import URLRequest
 from service.url_service import *
 from fastapi.testclient import TestClient
+from pydantic import ValidationError
 
 from main import app
 
@@ -11,26 +13,31 @@ client = TestClient(app)
 class TestUrlShortener(unittest.TestCase):
 
     def test_generate_short_url(self):
-        short_url = generate_short_url("https://example.com", None, 6)
+        short_url = generate_short_url("https://example.com", None, 10)
         self.assertIsNotNone(short_url)
-        self.assertEqual(len(short_url), 6)
+        self.assertEqual(len(short_url), 10)
     
+    @patch("models.pynamodb_model.UrlEntry.get")
     @patch("models.pynamodb_model.UrlEntry.save")
-    def test_generate_short_url_no_collision(self, mock_save):
-        short_url = generate_short_url("https://example.com", None, 6)
+    def test_generate_short_url_no_collision(self, mock_save, mock_get):
+        mock_get.side_effect = UrlEntry.DoesNotExist
+        mock_save.return_value = None
+        
+        short_url = generate_short_url("https://example.com", None, 10)
         self.assertIsNotNone(short_url)
-        self.assertEqual(len(short_url), 6)
+        self.assertEqual(len(short_url), 10)
         
     @patch("models.pynamodb_model.UrlEntry.get")
     @patch("models.pynamodb_model.UrlEntry.save")
     def test_generate_short_url_with_collision(self, mock_save, mock_get):
-        mock_get.side_effect = [UrlEntry(short_url="unique", original_url="https://example.com"), UrlEntry.DoesNotExist]
+        #simulate collision with first short url
+        mock_get.side_effect = [UrlEntry(short_url="unique_url", original_url="https://example.com"), UrlEntry.DoesNotExist]
         mock_save.return_value = None
         
-        short_url = generate_short_url("https://example.com", None, 6)
+        short_url = generate_short_url("https://example.com", None, 10)
         self.assertIsNotNone(short_url)
-        self.assertEqual(len(short_url), 6)
-        self.assertNotEqual(short_url, "unique")
+        self.assertEqual(len(short_url), 10)
+        self.assertNotEqual(short_url, "unique_url")
         
     @patch("models.pynamodb_model.UrlEntry.get")
     @patch("models.pynamodb_model.UrlEntry.save")    
@@ -50,6 +57,33 @@ class TestUrlShortener(unittest.TestCase):
         with self.assertRaises(ValueError) as context:
             generate_short_url("https://example.com", "mycustomurl")            
         self.assertIn("This custom URL is already in use.", str(context.exception))
+        
+        #test invalid url format
+    def test_generate_short_url_malformed(self):
+        with self.assertRaises(ValueError) as context:
+            generate_short_url("example.com")
+        self.assertIn("Invalid URL format", str(context.exception))
+        
+        #test get og url for nonexistent short url
+    @patch("models.pynamodb_model.UrlEntry.get")  
+    def test_get_original_url_nonexistent_short_url(self, mock_get):
+        mock_get.side_effect = UrlEntry.DoesNotExist
+        with self.assertRaises(ValueError) as context:
+            get_original_url("nonexistent")
+        self.assertIn("Short URL does not exist.", str(context.exception))
+        
+        #test invalid url length
+    def test_generate_short_url_invalid_length(self):
+        with self.assertRaises(ValidationError) as context:
+            URLRequest(url= "https://example.com", length=6)
+        self.assertEqual(context.exception.errors()[0]["msg"], "Input should be greater than or equal to 10")
+        
+       #test invalid custom url length
+    def test_generate_short_url_invalid_custom_str_length(self):
+        with self.assertRaises(ValidationError) as context:
+            URLRequest(url= "https://example.com", custom_url = "short")
+        self.assertEqual(context.exception.errors()[0]["msg"], "String should have at least 10 characters")    
+        
         
     @patch("models.pynamodb_model.UrlEntry.scan")
     def test_list_urls(self, mock_scan):

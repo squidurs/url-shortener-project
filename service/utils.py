7e0d00de60e2dcd1ed2d1a9cc42bb9e5a4634168
Sydney McClements <smccl96@gmail.com>
2024-11-13 23:05:37 -0800

    Updated get_current_user method signature to User object and fixed the formatting for updating a user entry in routes line 248

--- service/utils.py
+++ service/utils.py
@@ -101,7 +101,7 @@ def create_access_token(data: dict, expires_delta: timedelta):
     return encoded_jwt
 
 
-async def get_current_user(token: str = Depends(oauth_2_scheme)):
+async def get_current_user(token: str = Depends(oauth_2_scheme)) -> UserEntry:
     credential_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate credentials", headers={"WWW-Authenticate": "Bearer"})
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
